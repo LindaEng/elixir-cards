c177196e3f67cd4527abea738a407eff14c3e0b6
= <90lineng@gmail.com>
2023-05-08 16:20:30 -0400

    load method defined

--- lib/cards.ex
+++ lib/cards.ex
@@ -26,4 +26,23 @@ defmodule Cards do
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
+
+  def deal(deck, hand) do
+    Enum.split(deck, hand)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term binary
+      :error -> "File not found!!"
+    end
+
+  end
 end
